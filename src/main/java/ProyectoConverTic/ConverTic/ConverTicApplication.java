06a613b6467e7ef38ddae1449329810c0ed41328
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2023-01-04 15:54:05 -0500

    Arrancando proyecto

---
+++ src/main/java/ProyectoConverTic/ConverTic/ConverTicApplication.java
@@ -0,0 +1,13 @@
+package ProyectoConverTic.ConverTic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverTicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConverTicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProyectoConverTic/ConverTic/ConverTicApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoConverTic.ConverTic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverTicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
